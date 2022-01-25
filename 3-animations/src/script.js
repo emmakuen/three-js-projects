47a9a12262c6657212a1e534e3751c0098af0709
emmakuen <emmaku@protonmail.com>
2022-01-25 18:37:22 +0900

    add three.js animation practice

---
+++ 3-animations/src/script.js
@@ -0,0 +1,54 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Time
+let time = Date.now();
+
+// Animations
+const tick = () => {
+  // time
+  const currentTime = Date.now();
+  const deltaTime = currentTime - time;
+  time = currentTime;
+  // Update object
+  mesh.rotation.y += 0.001 * deltaTime; // adjust the animation speed based on the framerate
+  //   mesh.position.x += 0.001;
+  //   mesh.position.y += 0.001;
+  //   mesh.rotation.z += 0.001;
+  console.log("tick");
+
+  // Render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
